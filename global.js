68cfa464d96269bceafde874e3ef878e2917731d
jakemerlin1133 <jakemerlin123@gmail.com>
2024-01-31 15:18:49 +0800

    first commit

---
+++ global.js
@@ -0,0 +1,15 @@
+// global.setTimeout(() => {
+//     console.log('in the timeout');
+//     clearInterval(int);
+// }, 3000);
+
+
+// const int = setInterval(() => {
+//     console.log('in the interval')
+// }, 500)
+
+
+// console.log(__dirname);
+// console.log(__filename);
+
+console.log(document.querySelector);
